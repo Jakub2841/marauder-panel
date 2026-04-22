41a3cf3e212e0557603127f5928ed11a9c3c353b
Jakub <118729369Jakub2841@users.noreply.github.com>
2026-04-22 17:35:53 +0200

    fix: prevent logo distortion && removed unused sidebar variants

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -1,17 +1,5 @@
-@props([
-    'sidebar' => false,
-])
-
-@if($sidebar)
-    <flux:sidebar.brand {{ $attributes }}>
-        <x-slot name="logo">
-            <img src="{{ asset('logo.png') }}" alt="Logo" class="w-full h-auto max-h-24 rounded object-cover">
-        </x-slot>
-    </flux:sidebar.brand>
-@else
-    <flux:brand {{ $attributes }}>
-        <x-slot name="logo">
-            <img src="{{ asset('logo.png') }}" alt="Logo" class="size-8 rounded-md">
-        </x-slot>
-    </flux:brand>
-@endif
+<flux:brand {{ $attributes }}>
+    <x-slot name="logo">
+        <img src="{{ asset('logo.png') }}" alt="Logo" class="size-8 rounded-md">
+    </x-slot>
+</flux:brand>
